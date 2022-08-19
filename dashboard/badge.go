ed2638e3a350681668baf3d06c510fd2869dd29f
Arash Hatami <hatamiarash7@gmail.com>
2022-08-19 20:33:42 +0430

    remove unused codes

--- dashboard/badge.go
+++ dashboard/badge.go
@@ -1,19 +1,7 @@
 package dashboard
 
-import (
-	"github.com/hatamiarash7/gobadge/svg"
-	geo "github.com/hatamiarash7/gobadge/svg/geometry"
-)
-
 type Badge struct {
 	Label string `json:"label"`
 	Tag   string `json:"tag"`
 	Color string `json:"color"`
-}
-
-func (badge Badge) Draw(canvas *svg.Canvas) {
-	canvas.Open(float64(badgeW), float64(badgeH))
-	origin := geo.Coordinate{X: 0, Y: 0}
-	badge.draw(canvas, origin)
-	canvas.Close()
 }
--- dashboard/specs.go
+++
@@ -1,7 +0,0 @@
-package dashboard
-
-const (
-	badgeW    = 230 // badge width
-	badgeH    = 20  // badge height
-	blockfill = "#555555"
-)
