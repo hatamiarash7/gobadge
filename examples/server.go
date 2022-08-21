d0ca266f27a5eb96ca745069c32528e7d0f1339a
Arash Hatami <hatamiarash7@gmail.com>
2022-08-21 16:29:15 +0430

    Update server.go

--- examples/server.go
+++ examples/server.go
@@ -1,60 +1,42 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
+	"fmt"
 	"net/http"
 
 	dash "github.com/hatamiarash7/gobadge/dashboard"
 	"github.com/hatamiarash7/gobadge/svg"
 )
 
-func new(canvas *svg.Canvas) dash.View {
-
-	file, err := ioutil.ReadFile("data.json")
-
-	if err != nil {
-		log.Printf("readfile error %v", err)
-	}
-
-	var data dash.Badge
-
-	err = json.Unmarshal(file, &data)
-
-	if err != nil {
-		log.Printf("unmarshal error %v", err)
-	}
-
-	return dash.View{
-		Canvas: canvas,
-		Badge:  data,
-	}
-}
-
 // Build returns the dashboard badges as SVG.
-var dashboard = func(writer http.ResponseWriter, request *http.Request) {
+var dashboard = func(writer http.ResponseWriter, r *http.Request) {
+	label := r.URL.Query().Get("label")
+	tag := r.URL.Query().Get("tag")
+	color := r.URL.Query().Get("color")
 
 	writer.Header().Set("Content-Type", "image/svg+xml")
 	writer.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
 
 	canvas := svg.New(writer)
-	view := new(canvas)
+	view := dash.View{
+		Canvas: canvas,
+		Badge: dash.Badge{
+			Label: label,
+			Tag:   tag,
+			Color: color,
+		},
+	}
+
 	view.Draw()
 }
 
 const (
 	port  = ":8080"
-	route = "/dashboard/example"
+	route = "/example"
 )
 
 func main() {
-
-	// draw endpoint
 	http.HandleFunc(route, dashboard)
-
-	log.Printf("listening on http://localhost%s%s", port, route)
-
-	// use goroutine to serve endpoint
+	fmt.Printf("listening on http://localhost%s%s", port, route)
 	http.ListenAndServe(port, nil)
 }
